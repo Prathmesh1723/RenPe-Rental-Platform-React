9774d0939916e0905bf054b52aecd10a06ad79be
Pratik Thorat <thorat.p@northeastern.edu>
2022-12-08 00:31:30 -0500

    Added About Page Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar/navbar';
-import 'bootstrap/dist/css/bootstrap.css';
-import Form from './pages/form/form';
-import {BrowserRouter as Router, Routes, Route, Link, Switch} from 'react-router-dom';
+// import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "./pages/form/form";
+import About from "./pages/about/about.js";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
 
-
-import Home from './pages/home/home';
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        <Route path='/form' element={<Form />}></Route>
+        <Route path="/form" element={<Form />}></Route>
+        <Route path="/about" element={<About />}></Route>
         {/* <Route path='/Contact' element={<Contact />}></Route>
         <Route path='/Jobs' element={<Jobs />}></Route> */}
-        <Route path='/Home' element={<Home />}>
-        </Route>
+        <Route path="/Home" element={<Home />}></Route>
       </Routes>
     </Router>
   );
