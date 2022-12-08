ea8553094e4d9be899e8a0eec797fbe730da4b23
Pratik Thorat <thorat.p@northeastern.edu>
2022-12-08 03:10:02 -0500

    Added About us Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Login from './pages/login/login.component';
+import Login from "./pages/login/login.component";
 import Navbar from "./components/navbar/navbar";
 import "bootstrap/dist/css/bootstrap.css";
-import SignUp from './pages/signup/signup.component';
+import SignUp from "./pages/signup/signup.component";
 import Form from "./pages/form/form";
 import About from "./pages/about/about.js";
 import HowItWorks from "./pages/howItWorks/howItWorks";
@@ -22,11 +22,12 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        <Route path='/signup' element={<SignUp />}></Route>
-        <Route path='/login' element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         {/* <Route path='/Contact' element={<Contact />}></Route>
         <Route path='/Jobs' element={<Jobs />}></Route> */}
         <Route path="/Home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
         <Route path="/howItWorks" element={<HowItWorks />}></Route>
       </Routes>
     </Router>
